27763c69e07dbbcb21464fcf8d804d52ca55ef82
Nischal Shetty <nischal2004coc@gmail.com>
2024-03-12 13:56:55 +0530

    added api route

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { bullishS, bearishS } = require("./db");
+const { bullishS, bearishS } = require("../db");
 const dotenv = require("dotenv");
 dotenv.config();
 const cors = require("cors");
